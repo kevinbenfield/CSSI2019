4345e95fa10b3a301f118228ce60bebf6d4e8cbb
kevinbenfield <53183192+kevinbenfield@users.noreply.github.com>
2019-08-13 13:04:36 -0700

    Add files via upload

--- keepkalm/main.py
+++ keepkalm/main.py
@@ -7,16 +7,37 @@ jinja_env =  jinja2.Environment(
     extensions=['jinja2.ext.autoescape'],
     autoescape=True)
 
-    class MainPage(webapp2.RequestHandler):
-        def get(self):
-            t = jinja_env.get_template('/templates/index.html')
-            seed_data()
-            self.response.write(t.render())
 
-        def post(self):
-            t = jinja_env.get_template('/templates/index.html')
-            self.response.write(t.render())
+class KeepKalm(webapp2.RequestHandler):
+    def get(self):
+        t = jinja_env.get_template('/templates/index.html')
+        ()
+        self.response.write(t.render())
+
+    def post(self):
+        t = jinja_env.get_template('/templates/index.html')
+        self.response.write(t.render())
+
+class Response(webapp2.RequestHandler):
+    def get(self):
+        t = jinja_env.get_template('/templates/index.html')
+        seed_data()
+        self.response.write(t.render())
+
+    def post(self):
+        option=self.request.get('option')
+        option2=self.request.get('option2')
+        option3=self.request.get('option3')
+        option4=self.request.get('option4')
+        option5=self.request.get('option5')
 
 
-routes = [('/',MainPage)]
-app = webapp2.WSGIApplication(routes, debug=True)
+        d = {'option':option,'option2':option2,'option3':option3,'option4':option4,'option5':option5}
+        t = jinja_env.get_template('/templates/result.html')
+        self.response.write(t.render(d))
+
+
+app = webapp2.WSGIApplication([
+        ('/', KeepKalm),
+        ('/response', Response),
+        ], debug=True)
